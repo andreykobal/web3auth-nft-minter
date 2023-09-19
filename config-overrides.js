872aeeb906748d5e80812ce316a53ad50c1cba7d
Andreas Kobal <kobalandrey@gmail.com>
2023-09-20 00:51:02 +0100

    fixed errors webpack 5

--- config-overrides.js
+++ config-overrides.js
@@ -1,16 +1,28 @@
-const path = require('path');
+const webpack = require('webpack');
 
 module.exports = function override(config, env) {
     // Resolve the node modules for the required polyfills
     config.resolve.fallback = {
-        ...config.resolve.fallback,  // Make sure to spread the existing configuration
+        ...config.resolve.fallback,
         "crypto": require.resolve("crypto-browserify"),
         "url": require.resolve("url/"),
         "http": require.resolve("stream-http"),
         "https": require.resolve("https-browserify"),
         "zlib": require.resolve("browserify-zlib"),
-        "stream": require.resolve("stream-browserify"), 
+        "stream": require.resolve("stream-browserify")
+    };
 
+    // Provide the process shim
+    config.plugins.push(
+        new webpack.ProvidePlugin({
+            process: 'process/browser',
+        })
+    );
+
+    // Alias process to the installed package for browser compatibility
+    config.resolve.alias = {
+        ...config.resolve.alias,
+        process: "process/browser"
     };
 
     return config;
